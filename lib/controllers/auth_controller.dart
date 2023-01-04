de559479adec4c649ae7f61f69bdc56044161610
kimsungkyum <sungkyum1@naver.com>
2023-01-05 03:07:15 +0900

    refactor/콘트롤러, 서비스 분리

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,61 +1,23 @@
-import 'package:dio/dio.dart';
-import 'package:gdsc_mobile_project/data.dart';
-import 'package:gdsc_mobile_project/models/auth_body.dart';
-import 'package:gdsc_mobile_project/models/code_check_body.dart';
+import 'package:gdsc_mobile_project/services/auth_service.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 
 class AuthController extends GetxController {
-  String url = baseUrl + 'api/v1/authorizations';
-  String? code;
-  String? message;
+  final authService = AuthService();
 
   Future<bool> getCode(String phoneNum) async {
-    try {
-      final res = await Dio().post(
-        url,
-        data: {"auth": AuthBody(phone: phoneNum).toJson()},
-      );
-      print(res);
-      code = res.data['result']['code'];
-      message = res.data['result']['text'];
-      print(code);
-      return true;
-    } catch (e) {
-      print(e);
-      return false;
-    }
+    final result = await authService.getCode(phoneNum);
+    return result;
   }
 
   Future<bool> checkCode(String phone, String code) async {
-    try {
-      print('asf');
-      final res = await Dio().get(url, queryParameters: {
-        "auth": CodeCheckBody(code: code, phone: phone).toJson()
-      });
-      if (res.data['status'] == 'ok') {
-        print(res.data);
-        return true;
-      } else {
-        return false;
-      }
-    } catch (e) {
-      return false;
-    }
+    final result = await authService.checkCode(phone, code);
+    return result;
   }
 
   Future<bool> checkSignedUp(String phone) async {
-    try {
-      final res = await Dio().post(url + '/check_signed_up',
-          data: {'auth': AuthBody(phone: phone).toJson()});
-      if (res.data['status'] == 'ok') {
-        return res.data['terms_accepted_at'] == null ? false : true;
-      } else {
-        return false;
-      }
-    } catch (e) {
-      return false;
-    }
+    final result = await authService.checkSignedUp(phone);
+    return result;
   }
 
   void refreshCode() {}
