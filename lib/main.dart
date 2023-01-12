bf7bb4438fb4f474d526ad2aeade00938d65206b
kimsungkyum <sungkyum1@naver.com>
2023-01-12 23:45:10 +0900

    feat/splash screen 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gdsc_mobile_project/views/phone_verity_page.dart';
+import 'package:gdsc_mobile_project/views/splash_page.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.amber,
       ),
       debugShowCheckedModeBanner: false,
-      home: PhoneNumberVerificationPage(),
+      home: SplashPage(),
     );
   }
 }
